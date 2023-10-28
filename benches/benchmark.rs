30b488219d95d5a0df5755471663d6efe4b7fac8
visig <visig@protonmail.ch>
2023-10-28 17:36:59 +0800

    bench: add benchmarking for option cases

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -11,9 +11,7 @@ use core::{hint::black_box, iter::FusedIterator};
 use criterion::{criterion_group, criterion_main, Criterion};
 use first_err::FirstErr;
 
-mod l1 {
-    //! Tools for benchmark one layer `Result` iterator (`impl Iterator<Item = Result<_, _>>`)
-
+mod l1res {
     use super::*;
 
     /// One layer iterator.
@@ -81,8 +79,8 @@ mod l1 {
         let length: usize = 100_000;
 
         let group_name = match err_at {
-            Some(err_at) => format!("l1::err_at_{err_at:_<7}"),
-            None => format!("l1::err_not_exists"),
+            Some(err_at) => format!("l1res::err_at_{err_at:_<7}"),
+            None => format!("l1res::err_not_exists"),
         };
 
         // TEST: make sure answers are the same.
@@ -134,9 +132,7 @@ mod l1 {
     }
 }
 
-mod l2 {
-    //! Tools for benchmark tow layer `Result` iterator (`impl Iterator<Item = Result<Result<_, _>, _>>`)
-
+mod l2res {
     use super::*;
 
     /// Two layer iterator.
@@ -255,15 +251,15 @@ mod l2 {
 
         let group_name = match (l1_err_at, l2_err_at) {
             (Some(l1_err_at), Some(l2_err_at)) => {
-                format!("l2::l1_err_at_{l1_err_at:_<7}_l2_err_at_{l2_err_at:_<7}")
+                format!("l2res::l1_err_at_{l1_err_at:_<7}_l2_err_at_{l2_err_at:_<7}")
             }
             (Some(l1_err_at), None) => {
-                format!("l2::l1_err_at_{l1_err_at:_<7}_l2_err_not_exists")
+                format!("l2res::l1_err_at_{l1_err_at:_<7}_l2_err_not_exists")
             }
             (None, Some(l2_err_at)) => {
-                format!("l2::l1_err_not_exists_l2_err_at_{l2_err_at:_<7}")
+                format!("l2res::l1_err_not_exists_l2_err_at_{l2_err_at:_<7}")
             }
-            (None, None) => format!("l2::l1_err_not_exists_l2_err_not_exists"),
+            (None, None) => format!("l2res::l1_err_not_exists_l2_err_not_exists"),
         };
 
         // TEST: make sure answers are the same.
@@ -321,46 +317,393 @@ mod l2 {
     }
 }
 
+mod l1opt {
+    use super::*;
+
+    /// One layer iterator.
+    struct L1Iter {
+        curr: u64,
+        none_at: Option<u64>,
+    }
+
+    impl L1Iter {
+        fn new(none_at: Option<u64>) -> Self {
+            Self { curr: 0, none_at }
+        }
+    }
+
+    impl Iterator for L1Iter {
+        type Item = Option<u64>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let tmp = self.curr;
+            self.curr += 1;
+
+            let res = if Some(tmp) != self.none_at {
+                Some(Some(tmp))
+            } else {
+                Some(None)
+            };
+
+            // treat output of this iterator is a black box
+            black_box(res)
+        }
+    }
+
+    impl FusedIterator for L1Iter {}
+
+    /// The code implemented by first_err.
+    #[inline(never)]
+    fn first_err_approach(iter: impl Iterator<Item = Option<u64>>) -> Option<u64> {
+        iter.first_none_or_else(|iter1| iter1.sum::<u64>())
+    }
+
+    /// The code implemented by loop.
+    #[inline(never)]
+    fn loop_approach(iter: impl Iterator<Item = Option<u64>>) -> Option<u64> {
+        let mut sum = 0;
+        for opt in iter {
+            sum += opt?;
+        }
+
+        Some(sum)
+    }
+
+    /// The code implemented by `collect()`.
+    #[inline(never)]
+    fn collect_approach(iter: impl Iterator<Item = Option<u64>>) -> Option<u64> {
+        let sum = iter.collect::<Option<Vec<u64>>>()?.into_iter().sum::<u64>();
+
+        Some(sum)
+    }
+
+    /// Set L1 benchmark group by given arguments.
+    pub fn bench_setup(c: &mut Criterion, none_at: Option<u64>) {
+        let length: usize = 100_000;
+
+        let group_name = match none_at {
+            Some(none_at) => format!("l1opt::none_at_{none_at:_<7}"),
+            None => format!("l1opt::none_not_exists"),
+        };
+
+        // TEST: make sure answers are the same.
+        {
+            let collect_ans = black_box(collect_approach(black_box(
+                L1Iter::new(none_at).take(length),
+            )));
+
+            assert_eq!(
+                collect_ans,
+                black_box(loop_approach(black_box(L1Iter::new(none_at).take(length)))),
+                "loop approach test in: {group_name}",
+            );
+            assert_eq!(
+                collect_ans,
+                black_box(first_err_approach(black_box(
+                    L1Iter::new(none_at).take(length)
+                ))),
+                "first_err approach test in: {group_name}",
+            );
+        }
+
+        // benchmark conf
+        {
+            let mut group = c.benchmark_group(group_name);
+
+            group.bench_function("__collect", |b| {
+                b.iter(|| {
+                    black_box(collect_approach(black_box(
+                        L1Iter::new(none_at).take(length),
+                    )))
+                })
+            });
+
+            group.bench_function("_____loop", |b| {
+                b.iter(|| black_box(loop_approach(black_box(L1Iter::new(none_at).take(length)))))
+            });
+
+            group.bench_function("first_err", |b| {
+                b.iter(|| {
+                    black_box(first_err_approach(black_box(
+                        L1Iter::new(none_at).take(length),
+                    )))
+                })
+            });
+
+            group.finish();
+        }
+    }
+}
+
+mod l2opt {
+    use super::*;
+
+    /// Two layer iterator.
+    struct L2Iter {
+        curr: u64,
+        l1_none_at: Option<u64>,
+        l2_none_at: Option<u64>,
+    }
+
+    impl L2Iter {
+        fn new(l1_none_at: Option<u64>, l2_none_at: Option<u64>) -> Self {
+            Self {
+                curr: 0,
+                l1_none_at,
+                l2_none_at,
+            }
+        }
+    }
+
+    impl Iterator for L2Iter {
+        type Item = Result<Result<u64, u64>, u64>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let tmp = self.curr;
+            self.curr += 1;
+
+            // build inner Result<u64, u64>.
+            let l2_res = if Some(tmp) != self.l2_none_at {
+                Ok(tmp)
+            } else {
+                Err(tmp)
+            };
+
+            // build outer Result<Result<u64, u64>, u64>.
+            let l1_res = if Some(tmp) != self.l1_none_at {
+                Some(Ok(l2_res))
+            } else {
+                Some(Err(tmp))
+            };
+
+            // treat output of this iterator is a black box
+            black_box(l1_res)
+        }
+    }
+
+    impl FusedIterator for L2Iter {}
+
+    /// The code implemented by first_err.
+    #[inline(never)]
+    fn first_err_approach(
+        iter: impl Iterator<Item = Result<Result<u64, u64>, u64>>,
+    ) -> Result<u64, u64> {
+        iter.first_err_or_else(|iter1| iter1.first_err_or_else(|iter2| iter2.sum::<u64>()))
+            .and_then(|res| res)
+    }
+
+    /// The code implemented by loop.
+    #[inline(never)]
+    fn loop_approach(
+        mut iter: impl Iterator<Item = Result<Result<u64, u64>, u64>>,
+    ) -> Result<u64, u64> {
+        let mut sum = 0;
+        let mut inner_first_err: Option<u64> = None;
+
+        while let Some(outer_res) = iter.next() {
+            let inner_res = outer_res?; // return immediately when outer hit a `Err`.
+
+            match inner_res {
+                // no `Err` found for now (both inner and outer layer)
+                Ok(v) => {
+                    sum += v;
+                }
+
+                // this is inner's first `Err`.
+                Err(e) => {
+                    inner_first_err = Some(e);
+
+                    // inner_first_err already exists, we don't care anything further,
+                    // just verify all outer_res ASAP.
+                    for outer_res in iter {
+                        let _ = outer_res?;
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        // At this point, we're known no outer `Err` in iter.
+        if let Some(e) = inner_first_err {
+            return Err(e);
+        }
+
+        // no any `Err` (both inner and outer).
+        Ok::<u64, u64>(sum)
+    }
+
+    /// The code implemented by `collect()`.
+    #[inline(never)]
+    fn collect_approach(
+        iter: impl Iterator<Item = Result<Result<u64, u64>, u64>>,
+    ) -> Result<u64, u64> {
+        let sum = iter
+            .collect::<Result<Vec<Result<u64, u64>>, u64>>()?
+            .into_iter()
+            .collect::<Result<Vec<_>, _>>()?
+            .into_iter()
+            .sum::<u64>();
+
+        Ok::<u64, u64>(sum)
+    }
+
+    /// Set L2 benchmark group by given arguments.
+    pub fn bench_setup(c: &mut Criterion, l1_none_at: Option<u64>, l2_none_at: Option<u64>) {
+        let length: usize = 100_000;
+
+        let group_name = match (l1_none_at, l2_none_at) {
+            (Some(l1_none_at), Some(l2_none_at)) => {
+                format!("l2opt::l1_none_at_{l1_none_at:_<7}_l2_none_at_{l2_none_at:_<7}")
+            }
+            (Some(l1_none_at), None) => {
+                format!("l2opt::l1_none_at_{l1_none_at:_<7}_l2_none_not_exists")
+            }
+            (None, Some(l2_none_at)) => {
+                format!("l2opt::l1_none_not_exists_l2_none_at_{l2_none_at:_<7}")
+            }
+            (None, None) => format!("l2opt::l1_none_not_exists_l2_none_not_exists"),
+        };
+
+        // TEST: make sure answers are the same.
+        {
+            let collect_ans = black_box(collect_approach(black_box(
+                L2Iter::new(l1_none_at, l2_none_at).take(length),
+            )));
+
+            assert_eq!(
+                collect_ans,
+                black_box(loop_approach(black_box(
+                    L2Iter::new(l1_none_at, l2_none_at).take(length)
+                ))),
+                "loop approach test in: {group_name}",
+            );
+            assert_eq!(
+                collect_ans,
+                black_box(first_err_approach(black_box(
+                    L2Iter::new(l1_none_at, l2_none_at).take(length)
+                ))),
+                "first_err approach test in: {group_name}",
+            );
+        }
+
+        // benchmark conf
+        {
+            let mut group = c.benchmark_group(group_name);
+
+            group.bench_function("__collect", |b| {
+                b.iter(|| {
+                    black_box(collect_approach(black_box(
+                        L2Iter::new(l1_none_at, l2_none_at).take(length),
+                    )))
+                })
+            });
+
+            group.bench_function("_____loop", |b| {
+                b.iter(|| {
+                    black_box(loop_approach(black_box(
+                        L2Iter::new(l1_none_at, l2_none_at).take(length),
+                    )))
+                })
+            });
+
+            group.bench_function("first_err", |b| {
+                b.iter(|| {
+                    black_box(first_err_approach(black_box(
+                        L2Iter::new(l1_none_at, l2_none_at).take(length),
+                    )))
+                })
+            });
+
+            group.finish();
+        }
+    }
+}
+
 fn benchmarks(c: &mut Criterion) {
-    l1::bench_setup(c, Some(0));
-    l1::bench_setup(c, Some(10));
-    l1::bench_setup(c, Some(100));
-    l1::bench_setup(c, Some(1000));
-    l1::bench_setup(c, Some(10000));
-    l1::bench_setup(c, Some(99999));
-    l1::bench_setup(c, None);
+    // result
 
-    l2::bench_setup(c, Some(0), Some(1000));
-    l2::bench_setup(c, Some(10), Some(1000));
-    l2::bench_setup(c, Some(100), Some(1000));
-    l2::bench_setup(c, Some(1000), Some(1000));
-    l2::bench_setup(c, Some(10000), Some(1000));
-    l2::bench_setup(c, Some(99999), Some(1000));
-    l2::bench_setup(c, None, Some(1000));
+    l1res::bench_setup(c, Some(0));
+    l1res::bench_setup(c, Some(10));
+    l1res::bench_setup(c, Some(100));
+    l1res::bench_setup(c, Some(1000));
+    l1res::bench_setup(c, Some(10000));
+    l1res::bench_setup(c, Some(99999));
+    l1res::bench_setup(c, None);
 
-    l2::bench_setup(c, Some(1000), Some(0));
-    l2::bench_setup(c, Some(1000), Some(10));
-    l2::bench_setup(c, Some(1000), Some(100));
-    l2::bench_setup(c, Some(1000), Some(1000));
-    l2::bench_setup(c, Some(1000), Some(10000));
-    l2::bench_setup(c, Some(1000), Some(99999));
-    l2::bench_setup(c, Some(1000), None);
+    l2res::bench_setup(c, Some(0), Some(1000));
+    l2res::bench_setup(c, Some(10), Some(1000));
+    l2res::bench_setup(c, Some(100), Some(1000));
+    l2res::bench_setup(c, Some(1000), Some(1000));
+    l2res::bench_setup(c, Some(10000), Some(1000));
+    l2res::bench_setup(c, Some(99999), Some(1000));
+    l2res::bench_setup(c, None, Some(1000));
 
-    l2::bench_setup(c, None, Some(0));
-    l2::bench_setup(c, None, Some(10));
-    l2::bench_setup(c, None, Some(100));
-    l2::bench_setup(c, None, Some(1000));
-    l2::bench_setup(c, None, Some(10000));
-    l2::bench_setup(c, None, Some(99999));
+    l2res::bench_setup(c, Some(1000), Some(0));
+    l2res::bench_setup(c, Some(1000), Some(10));
+    l2res::bench_setup(c, Some(1000), Some(100));
+    l2res::bench_setup(c, Some(1000), Some(1000));
+    l2res::bench_setup(c, Some(1000), Some(10000));
+    l2res::bench_setup(c, Some(1000), Some(99999));
+    l2res::bench_setup(c, Some(1000), None);
 
-    l2::bench_setup(c, Some(0), None);
-    l2::bench_setup(c, Some(10), None);
-    l2::bench_setup(c, Some(100), None);
-    l2::bench_setup(c, Some(1000), None);
-    l2::bench_setup(c, Some(10000), None);
-    l2::bench_setup(c, Some(99999), None);
+    l2res::bench_setup(c, None, Some(0));
+    l2res::bench_setup(c, None, Some(10));
+    l2res::bench_setup(c, None, Some(100));
+    l2res::bench_setup(c, None, Some(1000));
+    l2res::bench_setup(c, None, Some(10000));
+    l2res::bench_setup(c, None, Some(99999));
 
-    l2::bench_setup(c, None, None);
+    l2res::bench_setup(c, Some(0), None);
+    l2res::bench_setup(c, Some(10), None);
+    l2res::bench_setup(c, Some(100), None);
+    l2res::bench_setup(c, Some(1000), None);
+    l2res::bench_setup(c, Some(10000), None);
+    l2res::bench_setup(c, Some(99999), None);
+
+    l2res::bench_setup(c, None, None);
+
+    // option
+
+    l1opt::bench_setup(c, Some(0));
+    l1opt::bench_setup(c, Some(10));
+    l1opt::bench_setup(c, Some(100));
+    l1opt::bench_setup(c, Some(1000));
+    l1opt::bench_setup(c, Some(10000));
+    l1opt::bench_setup(c, Some(99999));
+    l1opt::bench_setup(c, None);
+
+    l2opt::bench_setup(c, Some(0), Some(1000));
+    l2opt::bench_setup(c, Some(10), Some(1000));
+    l2opt::bench_setup(c, Some(100), Some(1000));
+    l2opt::bench_setup(c, Some(1000), Some(1000));
+    l2opt::bench_setup(c, Some(10000), Some(1000));
+    l2opt::bench_setup(c, Some(99999), Some(1000));
+    l2opt::bench_setup(c, None, Some(1000));
+
+    l2opt::bench_setup(c, Some(1000), Some(0));
+    l2opt::bench_setup(c, Some(1000), Some(10));
+    l2opt::bench_setup(c, Some(1000), Some(100));
+    l2opt::bench_setup(c, Some(1000), Some(1000));
+    l2opt::bench_setup(c, Some(1000), Some(10000));
+    l2opt::bench_setup(c, Some(1000), Some(99999));
+    l2opt::bench_setup(c, Some(1000), None);
+
+    l2opt::bench_setup(c, None, Some(0));
+    l2opt::bench_setup(c, None, Some(10));
+    l2opt::bench_setup(c, None, Some(100));
+    l2opt::bench_setup(c, None, Some(1000));
+    l2opt::bench_setup(c, None, Some(10000));
+    l2opt::bench_setup(c, None, Some(99999));
+
+    l2opt::bench_setup(c, Some(0), None);
+    l2opt::bench_setup(c, Some(10), None);
+    l2opt::bench_setup(c, Some(100), None);
+    l2opt::bench_setup(c, Some(1000), None);
+    l2opt::bench_setup(c, Some(10000), None);
+    l2opt::bench_setup(c, Some(99999), None);
+
+    l2opt::bench_setup(c, None, None);
 }
 
 criterion_group!(benches, benchmarks);
